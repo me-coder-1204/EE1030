9a828a50ae04d9f074ed0410ffbeba1bb2c817e0
me-coder-1204 <69784401+me-coder-1204@users.noreply.github.com>
2024-09-10 16:30:51 +0530

    changes

--- Assignment3/codes/line_gen.c
+++ Assignment3/codes/line_gen.c
@@ -13,11 +13,20 @@ int main(void){
 	B[0][0] = 6; B[1][0] = 4;
 	C[0][0] = 5; C[1][0] = -6;
 	D[0][0] = -3; D[1][0] = 5;
-
+	
+	
+	
 	FILE *file;
 	int len = 10;
 	file = fopen("./lines.dat","w");
 	for(int i = 0;i<=10;i++){
-		fprintf(file,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",A[0][0]+(B[0][0]-A[0][0])*i/(double)len,A[1][0]+(B[1][0]-A[1][0])*i/(double)len,A[0][0]+(C[0][0]-A[0][0])*i/(double)len,A[1][0]+(C[1][0]-A[1][0])*i/(double)len,A[0][0]+(D[0][0]-A[0][0])*i/(double)len,A[1][0]+(D[1][0]-A[1][0])*i/(double)len,B[0][0]+(C[0][0]-B[0][0])*i/(double)len,B[1][0]+(C[1][0]-B[1][0])*i/(double)len,B[0][0]+(D[0][0]-B[0][0])*i/(double)len,B[1][0]+(D[1][0]-B[1][0])*i/(double)len,C[0][0]+(D[0][0]-C[0][0])*i/(double)len,C[1][0]+(D[1][0]-C[1][0])*i/(double)len);
+		printMat(Matscale(A,2,1,i/10.0),2,1);
+		fprintf(file,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",Matadd(A,Matscale(Matsub(B,A,2,1),2,1,i/10.0),2,1)[0][0],Matadd(A,Matscale(Matsub(B,A,2,1),2,1,i/10.0),2,1)[1][0],Matadd(A,Matscale(Matsub(C,A,2,1),2,1,i/10.0),2,1)[0][0],Matadd(A,Matscale(Matsub(C,A,2,1),2,1,i/10.0),2,1)[1][0],Matadd(A,Matscale(Matsub(D,A,2,1),2,1,i/10.0),2,1)[0][0],Matadd(A,Matscale(Matsub(D,A,2,1),2,1,i/10.0),2,1)[1][0],Matadd(B,Matscale(Matsub(C,B,2,1),2,1,i/10.0),2,1)[0][0],Matadd(B,Matscale(Matsub(C,B,2,1),2,1,i/10.0),2,1)[1][0],Matadd(B,Matscale(Matsub(D,B,2,1),2,1,i/10.0),2,1)[0][0],Matadd(B,Matscale(Matsub(D,B,2,1),2,1,i/10.0),2,1)[1][0],Matadd(C,Matscale(Matsub(D,C,2,1),2,1,i/10.0),2,1)[0][0],Matadd(C,Matscale(Matsub(D,C,2,1),2,1,i/10.0),2,1)[1][0]);
 	}
+	
+	free(A);
+	free(B);
+	free(C);
+	free(D);
+	free(file);
 }
